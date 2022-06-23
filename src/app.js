553781c3cdf73b45dd37490b0ca7d305f08af7ed
sukanta <sukantasaha481@gmail.com>
2022-06-23 08:43:03 +0530

    config mail

--- src/app.js
+++ src/app.js
@@ -9,4 +9,17 @@ window.addEventListener("scroll", function () {
     menuToggle.classList.toggle("active");
     navigation.classList.toggle("active");
   }
-  
+
+  // function sendEmail(){
+  //   Email.send({
+  //     Host : "smtp.gmail.com",
+  //     Username : "sukantasaha481@gmail.com",
+  //     Password : "password",
+  //     To : 'sukantasaha481@gmail.com',
+  //     From : document.getElementById("email").value,
+  //     Subject : "This is New Enquiry",
+  //     Body : "And this is the body"
+  // }).then(
+  //   message => alert(message)
+  // );
+  // }
---
+++ src/mail.php
@@ -0,0 +1,18 @@
+
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "sukantasaha481@mail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From:" . $message. "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
